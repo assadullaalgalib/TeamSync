6ef00b3ca23260d0aec83943fb2d5b06bbe4b5bc
Md Asif Chowdhury <asifjarif@gmail.com>
2025-01-21 21:53:52 +0600

    Update db.php

--- model/db.php
+++ model/db.php
@@ -11,13 +11,6 @@ function getDbConnection() {
     $dbname = "teamsyncdb";
     
 
-    //Online Database Connection
-    // $dbhost = "sql113.infinityfree.com";
-    // $dbusername = "if0_38107243";
-    // $dbpassword = "tsdb1234";
-    // $dbname = "if0_38107243_teamsyncdb";
-    
-
     // Create connection
     $conn = new mysqli($dbhost, $dbusername, $dbpassword, $dbname);
     
