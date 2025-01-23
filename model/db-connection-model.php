d2d85b510ca24144ea27b4268d88ec16559939b8
Md Asif Chowdhury <asifjarif@gmail.com>
2025-01-24 03:38:35 +0600

    Add files via upload

--- model/db-connection-model.php
+++ model/db-connection-model.php
@@ -1,20 +1,15 @@
 <?php
-// db.php - Database connection file
 
 function getDbConnection() {
 
-    
     //Local Database Connection
     $dbhost = "localhost";
     $dbusername = "root";
     $dbpassword = "";
     $dbname = "teamsyncdb";
     
-
-    // Create connection
     $conn = new mysqli($dbhost, $dbusername, $dbpassword, $dbname);
     
-    // Check connection
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
     }
---
+++ model/session-manager-model.php
@@ -0,0 +1,35 @@
+<?php
+function startSession() {
+    if (session_status() == PHP_SESSION_NONE) {
+        session_start();
+    }
+}
+
+function setSession($key, $value) {
+    startSession();
+    $_SESSION[$key] = $value;
+}
+
+function getSession($key) {
+    startSession();
+    return isset($_SESSION[$key]) ? $_SESSION[$key] : null;
+}
+
+function sessionExists($key) {
+    startSession();
+    return isset($_SESSION[$key]);
+}
+
+function removeSession($key) {
+    startSession();
+    unset($_SESSION[$key]);
+}
+
+function destroySession() {
+    startSession();
+    session_unset();
+    session_destroy();
+}
+
+
+?>
