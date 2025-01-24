d114e9fba1daeec2aa1e81370ec21a48614bfa52
Md Asif Chowdhury <asifjarif@gmail.com>
2025-01-25 03:40:44 +0600

    Update user-login.php

--- view/user-login.php
+++ view/user-login.php
@@ -62,7 +62,7 @@ if (sessionExists('errorMessages')) {
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
     <title>TeamSync Login</title>
-    <link rel="stylesheet" href="../css/login.css">
+    <link rel="stylesheet" href="../css/user-login.css">
     <script src="../js/scripts.js" defer></script>
 </head>
 <body>
