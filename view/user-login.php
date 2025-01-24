4a5df48f46270d44e793461f3a67a3a28ef2bd1a
NoobMan <Meherafhasankhanrudro@gmail.com>
2025-01-25 00:18:27 +0600

    login css

--- view/user-login.php
+++ view/user-login.php
@@ -14,7 +14,7 @@ if (sessionExists('errorMessages')) {
 } */
 ?>
 <!-- here the validation should work so that it check if the email or pass is incorrect it will show the error using the PHP Validation -->
-<!DOCTYPE html>
+<!-- <!DOCTYPE html>
 <html>
 
 <head>
@@ -53,4 +53,36 @@ if (sessionExists('errorMessages')) {
     <p>Don't have an account? <a href="../view/user-registration.php">Register here</a></p>
 </body>
 
+</html> -->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+    <title>TeamSync Login</title>
+    <link rel="stylesheet" href="../css/login.css">
+    <script src="../js/scripts.js" defer></script>
+</head>
+<body>
+    <div class="login-box">
+        <h2>TeamSync Login</h2>
+        <form id="loginForm" action="../controller/user-login-controller.php" method="POST" onsubmit="loginValidate(event)">
+                <div class="input-group">
+                    <label for="email">Email:</label>
+                    <input type="text" name="email" id="email">
+                    <p class="error-message" id="emailError"></p>
+                </div>
+                <div class="input-group">
+                    <label for="password">Password:</label>
+                    <input type="password" name="password" id="password">
+                    <p class="error-message" id="passwordError"></p>
+                </div>
+                <button type="submit">Login</button>
+        </form>
+        <p>Don't have an account? <a href="../view/user-registration.php">Register here</a></p>
+    </div>
+</body>
 </html>
+
