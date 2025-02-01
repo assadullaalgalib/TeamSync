8c3cfe0d0f1bd36806824be78f685caf2a1340e5
NoobMan <Meherafhasankhanrudro@gmail.com>
2025-02-01 18:41:19 +0600

    Pmdashboard and pm show all proj css

--- view/pm-project-showall.php
+++ view/pm-project-showall.php
@@ -4,43 +4,33 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>All Projects - TeamSync</title>
-    <link rel="stylesheet" href="../css/styles.css">
+    <link rel="stylesheet" href="../css/pm-project-showall.css">
 </head>
 <body>
 
 <h1>All Projects</h1>
-<table border=1>
-    <thead>
-        <tr>
-            <th>Project ID</th>
-            <th>Client Name</th>
-            <th>Project Name</th>
-            <th>Description</th>
-            <th>Start Date</th>
-            <th>Deadline</th>
-            <th>Status</th>
-            <th>Progress</th>
-            <th>Client Feedback</th>
-        </tr>
-    </thead>
-    <tbody>
-        <?php foreach ($projects as $project) { ?>
-        <tr>
-            <td><?php echo $project['project_id']; ?></td>
-            <td><?php echo $project['client_name']; ?></td>
-            <td><?php echo $project['name']; ?></td>
-            <td><?php echo $project['description']; ?></td>
-            <td><?php echo $project['start_date']; ?></td>
-            <td><?php echo $project['deadline']; ?></td>
-            <td><?php echo $project['status']; ?></td>
-            <td><?php echo $project['progress']; ?>%</td>
-            <td><?php echo $project['client_feedback']; ?></td>
-        </tr>
-        <?php } ?>
-    </tbody>
-</table>
 
-<a href="../controller/user-dashboard-controller.php">Home</a>
+<div class="project-container">
+    <?php foreach ($projects as $project) { ?>
+        <div class="project-card">
+            <div class="card-header">
+                <span class="project-id">#<?php echo $project['project_id']; ?></span>
+                <span class="status" data-status="<?php echo $project['status']; ?>">
+                    <?php echo $project['status']; ?>
+                </span>
+            </div>
+            <h2><?php echo $project['name']; ?></h2>
+            <p>Client: <?php echo $project['client_name']; ?></p>
+            <p>Description: <?php echo $project['description']; ?></p>
+            <p>Start Date: <?php echo $project['start_date']; ?></p>
+            <p>Deadline: <?php echo $project['deadline']; ?></p>
+            <p>Progress: <span class="progress-text"><?php echo $project['progress']; ?>%</span></p>
+            <p>Client Feedback: <?php echo $project['client_feedback']; ?></p>
+        </div>
+    <?php } ?>
+</div>
+
+<a href="../controller/user-dashboard-controller.php" class="btn">Back to Dashboard</a>
 
 </body>
 </html>
