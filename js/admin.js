98e82aae317ce69470b2dd2afbfdda7ef8c96b2f
Md Asif Chowdhury <asifjarif@gmail.com>
2025-02-02 18:11:13 +0600

    Update admin.js

--- js/admin.js
+++ js/admin.js
@@ -1,3 +1,30 @@
+function showFileInput() {
+    document.getElementById('profile_picture').style.display = 'block';
+}
+
+function previewImage(event) {
+    var preview = document.getElementById('current-profile-pic');
+    var file = event.target.files[0];
+
+    if (file) {
+        var reader = new FileReader();
+        reader.onload = function () {
+            preview.src = reader.result;
+            preview.style.display = 'block';
+        };
+        reader.readAsDataURL(file);
+    } else {
+        preview.src = '';
+        preview.style.display = 'none';
+    }
+}
+
+function deleteProfilePicture() {
+    if (confirm('Are you sure you want to delete your profile picture?')) {
+        document.getElementById('delete-profile-picture-form').submit();
+    }
+}
+
 function setupSearch() {
     function loadDoc() {
         var xhr = new XMLHttpRequest();
