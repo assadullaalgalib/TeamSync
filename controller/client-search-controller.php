1ea50622aa376b567ae486b3dfabdeb263dbd250
jiniasama <jiniasama@gmail.com>
2025-02-03 01:57:14 +0600

    Update client-search-controller.php

--- controller/client-search-controller.php
+++ controller/client-search-controller.php
@@ -21,4 +21,15 @@ if (!empty($query)) {
         ];
     }
 }
+// Sort results to show best matches first
+usort($results, function($a, $b) {
+    return strcmp($a['name'], $b['name']);
+});
+
+// Limit the results to the first 5 items
+$limitedResults = array_slice($results, 0, 5);
+
+// Return the results as JSON
+echo json_encode($limitedResults);
+?>
 
