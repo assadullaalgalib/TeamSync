04df12158dfe78872551d92b9faffbfb707f94db
Md Asif Chowdhury <asifjarif@gmail.com>
2025-02-02 06:03:13 +0600

    Add files via upload

--- controller/dev-search-controller.php
+++ controller/dev-search-controller.php
@@ -3,6 +3,8 @@ include_once '../model/project-model.php';
 include_once '../model/task-model.php';
 include_once '../model/user-model.php';
 
+header('Content-Type: application/json');
+
 $query = $_GET['query'] ?? '';
 $filter = $_GET['filter'] ?? 'all';
 $roleid = $_GET['roleid'] ?? '';
@@ -29,10 +31,17 @@ usort($results, function($a, $b) {
     return strcmp($a['name'], $b['name']);
 });
 
-echo "<ul>";
+/* echo "<ul>";
 foreach ($results as $result) {
     $link = "../controller/dev-task-controller.php?action=view_active&task_id=" . $result['id'];
     echo "<li><a href='{$link}'>" . htmlspecialchars($result['formatted_name']) . "</a></li>";
 }
-echo "</ul>";
+echo "</ul>"; */
+
+
+// Limit the results to the first 5 items
+$limitedResults = array_slice($results, 0, 5);
+
+// Return the results as JSON
+echo json_encode($limitedResults);
 ?>
